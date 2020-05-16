d9d510894e7951d2690e342a5116d1f1452dadf6
mixailkop10 <64291143+mixailkop10@users.noreply.github.com>
2020-05-16 16:42:05 +0300

    Update Backer.cs

--- FundMeUp/Models/Backer.cs
+++ FundMeUp/Models/Backer.cs
@@ -9,7 +9,7 @@ namespace FundMeUp.Models
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Proffesion { get; set; }
+    public string Profession { get; set; }
     public string Address { get; set; }
     public string Email { get; set; } 
     public string Password { get; set; }
