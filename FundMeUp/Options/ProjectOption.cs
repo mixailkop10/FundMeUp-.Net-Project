5d13afadabf70bdabd2eb9d02b9048feb125a128
mixailkop10 <64291143+mixailkop10@users.noreply.github.com>
2020-05-16 16:45:15 +0300

    Update ProjectOption.cs

--- FundMeUp/Options/ProjectOption.cs
+++ FundMeUp/Options/ProjectOption.cs
@@ -11,14 +11,9 @@ namespace FundMeUp.Options
     public string Name { get; set; }
     public decimal BudgetGoal { get; set; }
     public decimal BalanceGoal { get; set; }
-    public List<BackerProject> BackerProject { get; set; }
-    public DateTime Doa { get; set; }
+    public DateTime DoA { get; set; }
     public Reward Reward { get; set; }
-    public int RewardId { get; set; }
-//    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public ProjectCreator ProjectCreator { get; set; }
-//    public bool Available { get; set; }
-//   public bool Funded { get; set; }
   }
 }
