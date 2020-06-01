2b231b89d266628426f0c522db48dbab1b96216e
Fotis Koumidis <fkoumidis@gmail.com>
2020-06-02 00:52:15 +0300

    add RewardService, IRewardService ,the dependency injection of this in MVC, modal for creating package

---
+++ FundMeUp/Services/RewardManager.cs
@@ -0,0 +1,65 @@
+﻿using FundMeUp.Models;
+using FundMeUp.Options;
+using FundMeUp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FundMeUp.Services
+{
+    public class RewardManager:IRewardManager
+    {
+        private FundMeUpDbContext db;
+
+        public RewardManager(FundMeUpDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Reward CreateReward(RewardOption rewardOpt)
+        {
+            Reward reward = new Reward
+            {
+                Name = rewardOpt.Name,
+                Description = rewardOpt.Description,
+                Price = rewardOpt.Price,
+                ProjectId = rewardOpt.ProjectId
+               
+
+            };
+
+            db.Rewards.Add(reward);
+            db.SaveChanges();
+
+            return reward;
+        }
+
+
+        public bool DeleteAllRewards(int projectId)
+        {
+            List<Reward> rewards = db.Rewards
+                .Where(r => r.ProjectId == projectId)
+                .ToList();
+            if (rewards != null)
+            { 
+            db.Rewards.RemoveRange(rewards);
+            db.SaveChanges();
+            return true;
+            }
+            return false;
+
+        }
+
+        public List<Reward> GetRewards(int projectId)
+        {
+            List<Reward> rewards = db.Rewards
+                .Where(r => r.ProjectId == projectId)
+                .ToList();
+            return rewards;
+        }
+
+        
+
+    }
+}
---
+++ FundMeUpMVC/Controllers/ApiRewardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FundMeUp.Models;
+using FundMeUp.Options;
+using FundMeUp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FundMeUpMVC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiRewardController : Controller
+    {
+        private IRewardManager rewardManager;
+        private readonly ILogger<ApiRewardController> _logger;
+
+        public ApiRewardController(ILogger<ApiRewardController> logger, IRewardManager _rewardManager)
+        {
+            rewardManager = _rewardManager;
+            _logger = logger;
+        }
+
+        [HttpPost("AddReward")]
+        public Reward CreateReward([FromBody] RewardOption projOpt)
+        {
+            return rewardManager.CreateReward(projOpt);
+        }
+    }
+}
