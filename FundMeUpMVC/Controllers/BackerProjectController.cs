a640eb238a7eb5db7388c54602673bbb4ced359b
elenanto <elenanto101@gmail.com>
2020-06-06 16:26:54 +0300

    Add UI for Backer's package reward submission

--- FundMeUpMVC/Controllers/BackerProjectController.cs
+++ FundMeUpMVC/Controllers/BackerProjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FundMeUp.Models;
+using FundMeUp.Options;
 using FundMeUp.Services;
 using FundMeUpMVC.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,16 +15,36 @@ namespace FundMeUpMVC.Controllers
     {
         private readonly ILogger<ProjectController> logger;
         private IBackerProjectManager bpMng;
+        private IRewardManager rMng;
 
-        public BackerProjectController(ILogger<ProjectController> logger, IBackerProjectManager bpMng)
+        public BackerProjectController(ILogger<ProjectController> logger, IBackerProjectManager bpMng, IRewardManager rMng)
         {
             this.logger = logger;
             this.bpMng = bpMng;
+            this.rMng = rMng;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("BackerProject/RewardPackageFund/{rewardid}/{backerid}")]
+        public IActionResult RewardPackageFund([FromRoute] int rewardid, [FromRoute] int backerid)
+        {
+            var reward = rMng.FindRewardById(rewardid);
+
+            FundingViewModel viewmodel = new FundingViewModel()
+            {
+                Reward = reward
+            };
+
+            if (reward != null) 
+            {
+                viewmodel.BackerProjectOption.ProjectId = viewmodel.Reward.ProjectId;  //exei ylopoihthei constructor sto FundingViewModel pou dhmiourgei to Backerprojectoption
+            }
+            viewmodel.BackerProjectOption.BackerId = 1;
+            return View(viewmodel);
         }
 
         public IActionResult StatusUpdate(int id)
---
+++ FundMeUpMVC/Models/FundingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FundMeUp.Models;
+using FundMeUp.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundMeUpMVC.Models
+{
+    public class FundingViewModel
+    {
+        public FundingViewModel()
+        {
+            this.BackerProjectOption = new BackerProjectOption();
+        }
+        public BackerProjectOption BackerProjectOption { get; set; }
+        public Reward Reward { get; set; }
+    }
+}
